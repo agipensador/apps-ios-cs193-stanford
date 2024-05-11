fa636147db6e1506a67d1701133ff8413aa43429
agipensador <agipensador@gmail.com>
2024-05-11 02:33:40 -0300

    initial commit cardviews isFaceUp

---
+++ memorize/memorize/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by giovanni on 10/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+        }.foregroundColor(.indigo).padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView : View {
+    var isFaceUp: Bool = false
+    var body: some View{
+        ZStack (content: {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else{
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
---
+++ memorize/memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by giovanni on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
