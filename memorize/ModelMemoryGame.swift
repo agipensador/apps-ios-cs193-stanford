174461215f8783b8ab8d2d2870606ba16dcf6689
agipensador <agipensador@gmail.com>
2024-05-17 00:51:56 -0300

    flip 2 cards refactor code

--- memorize/ModelMemoryGame.swift
+++ memorize/ModelMemoryGame.swift
@@ -20,29 +20,48 @@ struct ModelMemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
-    mutating func choose(_ card: Card){
-    
-        let chooseIndex = index(of: card)
-        cards[chooseIndex].isFaceUp.toggle()
-      }
-    
-    func index(of card: Card) -> Int{
-        for index in cards.indices{
-            if cards[index].id == card.id{
-                return index
-            }
-        }
-        return 0 // FIXME: Bogus!
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+//            if faceUpCardItems.count == 1 {
+//                return faceUpCardItems.first
+//            } else {
+//                return nil
+//            }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
+//            for index in cards.indices {
+//                if index == newValue {
+//                    cards[index].isFaceUp = true
+//                } else {
+//                    cards[index].isFaceUp = false
+//                }
+//            }
     }
     
+    mutating func choose(_ card: Card){
+        if let chooseIndex = cards.firstIndex(where:{ $0.id == card.id}){
+            if !cards[chooseIndex].isFaceUp && !cards[chooseIndex].isMatched{
+                if let potentialMatchedIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chooseIndex].content == cards[potentialMatchedIndex].content {
+                        cards[chooseIndex].isMatched = true
+                        cards[potentialMatchedIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chooseIndex
+                }
+                cards[chooseIndex].isFaceUp = true
+                
+            }
+        }
+      }
+        
     mutating func shuffle(){
         cards.shuffle()
         print(cards)
     }
-    
+        
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible{
         
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         var content: CardContent
         
@@ -51,5 +70,11 @@ struct ModelMemoryGame<CardContent> where CardContent: Equatable {
         var debugDescription: String {
             "\(id) \(content) \(isFaceUp ? "cima" : "baixo")"
         }
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
     }
 }
