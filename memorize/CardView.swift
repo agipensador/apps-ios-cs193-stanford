88f44dc6f8cf92cc36ccc2d4d7b5b9d0326688ca
agipensador <agipensador@gmail.com>
2024-05-22 18:42:30 -0300

    create Cardify how to compress code

--- memorize/CardView.swift
+++ memorize/CardView.swift
@@ -16,35 +16,21 @@ struct CardView : View {
     }
     
     var body: some View{
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
-            Group{
-                base.fill(.white)
-                base.strokeBorder(lineWidth: Constants.lineWidth)
-                Path{
-                    p in
-                    p.move(to: .zero)
-                    p.addLine(to: CGPoint(x: 100, y: 100))
-                }.stroke(lineWidth: 6)
-                Pie(endAngle: .degrees(240))
-                    .opacity(Constants.Pie.opacity)
-                    .overlay(
-                        Text(card.content)
-                            .font(.system(size: Constants.FontSize.largest))
-                            .minimumScaleFactor(Constants.FontSize.scaleFactor)
-                            .aspectRatio(1, contentMode: ContentMode.fit)
-                            .multilineTextAlignment(.center)
-                            .padding(Constants.Pie.insent)
-                        ).padding(Constants.insent)
-                }.opacity(card.isFaceUp ? 1 : 0)
-            base.fill().opacity(card.isFaceUp ? 0 : 1)
-        }
+        Pie(endAngle: .degrees(240))
+            .opacity( Constants.Pie.opacity)
+            .overlay(
+                Text(card.content)
+                    .font(.system(size:Constants.FontSize.largest))
+                    .minimumScaleFactor(Constants.FontSize.scaleFactor)
+                    .multilineTextAlignment(.center)
+                    .aspectRatio(1, contentMode: ContentMode.fit)
+                    .padding(Constants.Pie.insent)
+                ).padding(Constants.insent)
+            .modifier(Cardify(isFaceUp: card.isFaceUp))
         .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
     
     private struct Constants {
-        static let cornerRadius: CGFloat = 12
-        static let lineWidth: CGFloat = 2
         static let insent: CGFloat = 5
         struct FontSize{
             static let largest: CGFloat = 200
--- memorize/memorize/ Pie.swift
+++ memorize/memorize/ Pie.swift
@@ -22,7 +22,6 @@ struct Pie: Shape {
             x: center.x * radius * cos(startAngle.radians),
             y: center.y * radius * sin(startAngle.radians)
         )
-        
         var p = Path()
         p.move(to: center)
         p.addLine(to: start)
@@ -31,10 +30,11 @@ struct Pie: Shape {
             radius: radius,
             startAngle: startAngle,
             endAngle: endAngle,
-            clockwise: !clockwise)
+            clockwise: !clockwise
+        )
         p.addLine(to: center)
-        
-        return p 
+
+        return p
     }
     
      
